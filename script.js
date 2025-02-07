084f416c2997db672e18987e19dd7cea07421a14
Oukangneng <oaoconnell@gmail.com>
2025-02-06 22:37:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -29,13 +29,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Define political prisoner locations
     const locations = [
-        { coords: [35.8617, 104.1954], zoom: 4, text: "Overview of political prisoners in China" },
-        { coords: [39.9042, 116.4074], zoom: 10, text: "Political Prisoner 1 - Beijing" },
-        { coords: [31.2304, 121.4737], zoom: 10, text: "Political Prisoner 2 - Shanghai" },
-        { coords: [23.1291, 113.2644], zoom: 10, text: "Political Prisoner 3 - Guangzhou" },
-        { coords: [29.5630, 106.5516], zoom: 10, text: "Political Prisoner 4 - Chongqing" },
-        { coords: [41.8057, 123.4315], zoom: 10, text: "Political Prisoner 5 - Shenyang" }
-    ];
+    { coords: [35.8617, 104.1954], zoom: 4, text: "Overview of political prisoners in China" }, // Start
+    { coords: [30.657, 104.066], zoom: 8, text: "Zooming into Sichuan Province" }, // Sichuan Zoom
+    { coords: [30.578, 103.947], zoom: 12, text: "Political Prisoner 1 - Sichuan" }, // First prisoner in Sichuan
+    { coords: [31.2304, 121.4737], zoom: 10, text: "Political Prisoner 2 - Shanghai" },
+    { coords: [23.1291, 113.2644], zoom: 10, text: "Political Prisoner 3 - Guangzhou" },
+    { coords: [29.5630, 106.5516], zoom: 10, text: "Political Prisoner 4 - Chongqing" },
+    { coords: [41.8057, 123.4315], zoom: 10, text: "Political Prisoner 5 - Shenyang" }
+];
+
 
     // Create markers and hide them initially
     const markers = locations.map(({ coords, text }) => {
